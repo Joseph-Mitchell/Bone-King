090cac4edb416c1b72bc16d6a2eb539873f105ad
Joseph <joejmitchell@hotmail.co.uk>
2024-02-09 17:56:58 +0000

    Update Scores to use Sprite object

--- Bone King/Scores.cs
+++ Bone King/Scores.cs
@@ -5,8 +5,7 @@ namespace Bone_King
 {
     class Scores
     {
-        Texture2D texture;
-        Vector2 position;
+        Sprite sprite;
 
         float frameTimer;
         public bool active;
@@ -14,28 +13,28 @@ namespace Bone_King
         const float ACTIVETIME = 60;
         public Scores(Texture2D texture, int x, int y)
         {
-            this.texture = texture;
-            position = new Vector2(x, y);
+            sprite = new Sprite(new Vector2(x, y), 1)
+            {
+                centerOrigin = true
+            };
 
             active = true;
             frameTimer = ACTIVETIME;
+
+            sprite.Load(texture);
         }
 
         public void Update()
         {
-            if (frameTimer <= 0)
-            {
-                active = false;
-            }
-            else
-            {
+            if (frameTimer > 0)
                 frameTimer -= 1;
-            }
+            else
+                active = false;
         }
 
-        public void Draw(SpriteBatch sb)
+        public void Draw(SpriteBatch spriteBatch)
         {
-            sb.Draw(texture, position, null, Color.White, 0, new Vector2(texture.Bounds.Center.X, texture.Bounds.Center.Y), 1, SpriteEffects.None, 1);
+            sprite.Draw(spriteBatch);
         }
     }
 }
